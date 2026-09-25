1b6851c66a102a6f84c0ec1a35c9616962950943
erkan-tekin-49-66 <erkantekin696@gmail.com>
2026-09-25 15:05:34 +0300

    Add setup.py for yom-prime package
    Initial setup configuration for the yom-prime package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup
+    (name="yom-prime",
+    version="0.1.0",
+    author="Erkan Tekin",
+    description="YOM Prime Theorem - pi(x)=x/(ln(x)-6/7) - 6/7 correction",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    py_modules=["yom-prime"],
+    license="GPL-3.0",
+    url="https://github.com/erkan-tekin-49-66/yom-prime")
