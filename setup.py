38bdb3a8e592ba115e633dec1df1afd3dfcf3e02
erkan-tekin-49-66 <erkantekin696@gmail.com>
2026-09-25 15:35:29 +0300

    Fix module name from 'yom-prime' to 'yom_prime'

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup
     description="YOM Prime Theorem - pi(x)=x/(ln(x)-6/7) - 6/7 correction",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    py_modules=["yom-prime"],
+    py_modules=["yom_prime"],
     license="GPL-3.0",
     url="https://github.com/erkan-tekin-49-66/yom-prime")
